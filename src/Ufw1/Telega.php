0dc507e498416110ab8690554fdb1e0c60d5c066
hex@umonkey.net <hex@umonkey.net>
2019-10-24 17:41:53 +0300

    Telega: logs and proxy

--- src/Ufw1/Telega.php
+++ src/Ufw1/Telega.php
@@ -16,14 +16,32 @@ class Telega
 
     public function sendMessage($text)
     {
+        $logger = $this->container->get('logger');
+
+        if (!function_exists('curl_init')) {
+            $logger->warning('telega: curl not available.');
+            return false;
+        }
+
         $st = $this->container->get('settings');
         if (empty($st['telega']['bot_id']) or empty($st['telega']['chat_id'])) {
-            // TODO: add some logging, maybe?
+            $logger->warning('telega: bot/chat not set.');
             return false;
         }
 
         $url = sprintf('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s',
             $st['telega']['bot_id'], $st['telega']['chat_id'], urlencode($text));
+
+        $ch = curl_init();
+        curl_setopt($ch, \CURLOPT_URL, $url);
+
+        if (isset($st['telega']['proxy'])) {
+            curl_setopt($ch, \CURLOPT_PROXYTYPE, \CURLPROXY_SOCKS5);
+            curl_setopt($ch, \CURLOPT_PROXY, $st['telega']['proxy']);
+        }
+
+        $res = curl_exec($ch);
+        curl_close($ch);
 
         $res = file_get_contents($url);
 
