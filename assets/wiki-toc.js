96e443033f87bf25555cc0dc0607817bd182b6aa
hex@umonkey.net <hex@umonkey.net>
2019-10-26 23:01:06 +0300

    Wiki: added back toc button

---
+++ assets/wiki-toc.js
@@ -0,0 +1,3 @@
+jQuery(function ($) {
+    $(".wiki_buttons").append('<li><a class="btn btn-default tool" data-action="toc" href="/wiki?name=wiki:оглавление" title="Вставить оглавление" target="_blank"><i class="fa fa-list-ol"></i></a></li>');
+});
