6a1f09a44309e66afdd96fc88233771f572a6ec9
hex@umonkey.net <hex@umonkey.net>
2019-10-26 23:00:53 +0300

    Wiki: added back map button

---
+++ assets/wiki-map.js
@@ -0,0 +1,3 @@
+jQuery(function ($) {
+    $(".wiki_buttons").append('<li><a class="btn btn-default tool" href="/wiki?name=Как вставить карту" title="Вставить карту" target="_blank"><i class="fa fa-map-marker"></i></a></li>');
+});
