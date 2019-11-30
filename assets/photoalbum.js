5f3d9586361c827e82b44d2d9fca3b87eda7b092
hex@umonkey.net <hex@umonkey.net>
2019-11-30 22:17:05 +0300

    assets: photoalbum

---
+++ assets/photoalbum.js
@@ -0,0 +1,35 @@
+/**
+ * Photoalbum animation.
+ * Shows the more-less buttons.
+ **/
+jQuery(function ($) {
+    $(".photoalbum").each(function () {
+        var width = 0,
+            max_width = $(this).width() - 35;
+
+        $(this).find("a.image").each(function () {
+            width += $(this).width() + 5;
+            if (width >= max_width)
+                $(this).addClass("overflow");
+        });
+
+        var of = $(this).find("a.image.overflow");
+        if (of.length > 0) {
+            $(this).append("<div class='icon showmore'><i class='fas fa-chevron-circle-right'></i></div>");
+            $(this).append("<div class='icon showless'><i class='fas fa-chevron-circle-left'></i></div>");
+        }
+
+        // show everything, without flickering
+        $(this).fadeTo(100, 1.0);
+    });
+
+    $(document).on("click", ".photoalbum .showmore", function (e) {
+        var album = $(this).closest(".photoalbum");
+        album.toggleClass("open");
+    });
+
+    $(document).on("click", ".photoalbum .showless", function (e) {
+        var album = $(this).closest(".photoalbum");
+        album.toggleClass("open");
+    });
+});
