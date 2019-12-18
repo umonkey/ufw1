815c018a2ae3c1b51a7626e742b848069a8e70bc
hex@umonkey.net <hex@umonkey.net>
2019-12-18 08:52:27 +0300

    Fixed regitering new users

--- src/Ufw1/Handlers/Account.php
+++ src/Ufw1/Handlers/Account.php
@@ -70,69 +70,83 @@ class Account extends CommonHandler
         }
     }
 
+    /**
+     * Displays and handles the user registration form.
+     **/
     public function onRegister(Request $request, Response $response, array $args)
     {
         $user = $this->getUser($request);
 
         if ($request->getMethod() == "POST") {
-            $form = array_merge([
-                "name" => null,
-                "first_name" => null,
-                "last_name" => null,
-                "email" => null,
-                "password" => null,
-                "next" => null,
-            ], $request->getParsedBody());
-
-            if (empty($form['name']) and !empty($form['first_name']))
-                $form['name'] = $form['last_name'] . ' ' . $form['first_name'];
-
-            $this->checkRegisterForm($form);
-
-            $nodes = $this->node->where("`type` = 'user' AND `key` = ?", [$form["email"]]);
-            if (count($nodes))
-                $this->fail("Пользователь с таким адресом уже есть.");
-
-            $node = array_merge($form, [
-                "type" => "user",
-                "published" => 0,
-                "role" => "nobody",
-                "password" => password_hash($form["password"], PASSWORD_DEFAULT),
-            ]);
-
-            // Первый регистрируемый пользователь всегда администратор.
-            $userCount = (int)$this->db->fetchcell("SELECT COUNT(*) FROM `nodes` WHERE `type` = 'user'");
-            if ($userCount == 0) {
-                $node["published"] = 1;
-                $node["role"] = "admin";
-            }
-
-            $node = $this->node->save($node);
-
-            $this->sessionSave($request, [
-                "user_id" => $node["id"],
-            ]);
-
-            $this->notifyAdmin($request, $node);
-
-            return $response->withJSON([
-                "redirect" => $form["next"] ? $form["next"] : "/",
-            ]);
+            return $this->onRegisterNew($request, $response, $args);
         }
 
         if ($user) {
-            $back = $_GET["back"] ?? "/";
+            $back = $_GET["back"] ?: "/";
             return $response->withRedirect($back);
         }
 
         return $this->render($request, "register.twig");
     }
 
+    public function onRegisterNew(Request $request, Response $response, array $args)
+    {
+        $this->db->beginTransaction();
+
+        $form = array_merge([
+            "name" => null,
+            "first_name" => null,
+            "last_name" => null,
+            "email" => null,
+            "password" => null,
+            "next" => null,
+        ], $request->getParsedBody());
+
+        if (empty($form['name']) and !empty($form['first_name'])) {
+            $form['name'] = $form['last_name'] . ' ' . $form['first_name'];
+        }
+
+        $this->checkRegisterForm($form);
+
+        $nodes = $this->node->where("`type` = 'user' AND `key` = ?", [$form["email"]]);
+        if (count($nodes)) {
+            $this->fail("Пользователь с таким адресом уже есть.");
+        }
+
+        $node = array_merge($form, [
+            "type" => "user",
+            "published" => 0,
+            "role" => "nobody",
+            "password" => password_hash($form["password"], PASSWORD_DEFAULT),
+        ]);
+
+        // Первый регистрируемый пользователь всегда администратор.
+        $userCount = (int)$this->db->fetchcell("SELECT COUNT(*) FROM `nodes` WHERE `type` = 'user'");
+        if ($userCount == 0) {
+            $node["published"] = 1;
+            $node["role"] = "admin";
+        }
+
+        $node = $this->node->save($node);
+
+        $this->sessionSave($request, [
+            "user_id" => $node["id"],
+        ]);
+
+        $this->notifyAdmin($request, $node);
+
+        $this->db->commit();
+
+        return $response->withJSON([
+            "redirect" => $form["next"] ? $form["next"] : "/",
+        ]);
+    }
+
     protected function checkRegisterForm(array &$form)
     {
         $require = [
             "name" => "Не указано имя.",
-            "email" => "Не указан почтовый адрес.",
+            "email" => "Не указан адрес электронной почты.",
             "password" => "Не задан пароль.",
         ];
 
