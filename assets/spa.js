e7cb6e4b4bd6e8b9fc4e1a5849c82177dacea769
hex@umonkey.net <hex@umonkey.net>
2020-01-09 22:05:57 +0300

    SPA improvements

--- assets/spa.js
+++ assets/spa.js
@@ -4,15 +4,52 @@
  * Not really.  Just load local pages with XHR and update page contents.
  * Fires custon "ufw:reload" event to re-initialize controls.
  * Drastically improves page load speed.
+ *
+ * TODO: scroll restoration doesn't work, hard-coded 0 for now.
  **/
 jQuery(function ($) {
     const SELECTOR = '#body';
 
-    var openURL = function (link, callback) {
+    var fqurl = function (link) {
+        var a = document.createElement('a');
+        a.href = link;
+        return a.href;
+    };
+
+    var scrollTo = function (link) {
+        var parts = link.split('#', 2);
+
+        if (parts.length == 1) {
+            console.log('scrolling to top');
+            window.scroll({top: 0});
+        } else {
+            var l = $('#' + parts[1]);
+
+            if (l.length == 1) {
+                var y = l.offset().top;
+                console.log('scrolling to ' + y);
+                window.scroll({top: y});
+            } else {
+                console.log('cannot scroll to', link);
+            }
+        }
+    };
+
+    var openURL = function (link, scroll, callback) {
         var bars = $('a.bars i'),
             cls = bars.attr('class');
 
         bars.attr('class', 'fas fa-spinner fa-spin');
+
+        if (link[0] == '#') {
+            scrollTo(link);
+
+            if (callback) {
+                callback();
+            }
+
+            return;
+        }
 
         // Load contents.
         $.ajax({
@@ -31,6 +68,8 @@ jQuery(function ($) {
                 $(SELECTOR).replaceWith(d);
                 $(document).trigger('ufw:reload');
 
+                scrollTo(link);
+
                 if (callback) {
                     callback();
                 }
@@ -48,11 +87,6 @@ jQuery(function ($) {
             return;
         }
 
-        // Local link, pass.
-        if (link[0] == '#') {
-            return;
-        }
-
         // New tab, pass.
         if ($(this).attr('target')) {
             return;
@@ -63,18 +97,33 @@ jQuery(function ($) {
             return;
         }
 
-        openURL(link, function () {
-            window.history.pushState({link: link}, '', link);
+        // Update current history entry to save scroll position.
+        window.history.replaceState({
+            link: window.location.href,
+            scrollTop: $(window).scrollTop()
+        }, '', window.location.href);
+
+        openURL(link, 0, function () {
+            window.history.pushState({
+                link: link,
+                scrollTop: $(window).scrollTop()
+            }, '', link);
         });
 
         e.preventDefault();
     });
 
     window.addEventListener('popstate', function (e) {
+        console.log('popstate', e.state ? e.state.link : null);
+
         if (e.state !== null) {
             openURL(e.state.link);
         } else {
             openURL(document.location.href);
         }
     });
+
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
 });
