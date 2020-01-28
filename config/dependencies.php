1ddf7a449f39a9ca2534db0958c00600d7fc86f5
hex@umonkey.net <hex@umonkey.net>
2020-01-28 23:21:41 +0300

    Better comments for default dependencies

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,5 +1,13 @@
 <?php
-// DIC configuration
+/**
+ * Dependency setup.
+ **/
+
+declare(strict_types=1);
 
 $container = $app->getContainer();
-\Ufw1\Util::containerSetup($container);
+
+// $container['foo'] = function ($c) {
+//     $bar = $c->get('bar');
+//     return new Foo\Bar($bar);
+// };
