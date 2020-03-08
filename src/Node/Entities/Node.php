9e25c928ca720ca0321e7f14e4527163d724ddf8
hex@umonkey.net <hex@umonkey.net>
2020-03-08 20:50:25 +0300

    Node::fromArray()

--- src/Node/Entities/Node.php
+++ src/Node/Entities/Node.php
@@ -12,6 +12,18 @@ use ArrayObject;
 
 class Node extends ArrayObject
 {
+    public static function fromArray(array $props): self
+    {
+        switch ($props['type']) {
+            case 'user':
+                return new User($props);
+            case 'file':
+                return new File($props);
+            default:
+                return new static($props);
+        }
+    }
+
     public function update(array $props): void
     {
         foreach ($props as $k => $v) {
